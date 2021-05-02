19fe648aa655927cb9c232c86716a028d38e1d4c
Debanu Gupta <debanugupta@gmail.com>
2021-05-02 20:54:03 +0530

    changes 242

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import { Fragment } from 'react';
-import Auth from './components/Auth';
 import Counter from './components/Counter';
 import Header from './components/Header';
 
 
 function App() {
   return (
-    <Fragment>
-      <Header />
-      <Auth />
-      <Counter />
-    </Fragment>
+
+    <Counter />
+
   );
 }
 
